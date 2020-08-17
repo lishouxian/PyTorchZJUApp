b79e23d0962d443702f8d009c7c2fdc2ea0d7514
lishouxian <li.shouxian@outlook.com>
2020-08-17 10:25:05 +0800

    添加执行拍照和选取照片的操作

--- app/src/main/java/org/pytorch/demo/vision/VisionListActivity.java
+++ app/src/main/java/org/pytorch/demo/vision/VisionListActivity.java
@@ -1,14 +1,20 @@
 package org.pytorch.demo.vision;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 
 import org.pytorch.demo.AbstractListActivity;
 import org.pytorch.demo.InfoViewFactory;
 import org.pytorch.demo.R;
 
-public class VisionListActivity extends AbstractListActivity {
+import java.io.IOException;
 
+public class VisionListActivity extends AbstractListActivity {
+  int cameraRequestCode = 001;
+  int cameraRequestCode2 = 002;
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
@@ -22,25 +28,39 @@ public class VisionListActivity extends AbstractListActivity {
     });
 
     findViewById(R.id.take_a_photo).setOnClickListener(v -> {
-      final Intent intent = new Intent(VisionListActivity.this, ImageClassificationActivity.class);
-      intent.putExtra(ImageClassificationActivity.INTENT_MODULE_ASSET_NAME, "new.pt");
-      intent.putExtra(ImageClassificationActivity.INTENT_INFO_VIEW_TYPE,
-          InfoViewFactory.INFO_VIEW_TYPE_IMAGE_CLASSIFICATION_RESNET);
-      startActivity(intent);
+
+      Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+      startActivityForResult(cameraIntent,cameraRequestCode);
+
     });
+
 
     findViewById(R.id.select_a_picture).setOnClickListener(v -> {
-      final Intent intent = new Intent(VisionListActivity.this, ImageClassificationActivity.class);
-      intent.putExtra(ImageClassificationActivity.INTENT_MODULE_ASSET_NAME, "new.pt");
-      intent.putExtra(ImageClassificationActivity.INTENT_INFO_VIEW_TYPE,
-              InfoViewFactory.INFO_VIEW_TYPE_IMAGE_CLASSIFICATION_RESNET);
-      startActivity(intent);
+      Intent intent = new Intent();
+      intent.setAction(Intent.ACTION_GET_CONTENT);
+      intent.setType("image/*");
+      startActivityForResult(intent, cameraRequestCode2);
+
     });
 
+  }
 
+  @Override
+  protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 
+    super.onActivityResult(requestCode, resultCode, data);
+    if (requestCode == cameraRequestCode && resultCode == RESULT_OK) {
+
+      Intent resultView = new Intent(this, Result.class);
+
+      resultView.putExtra("imagedata",data.getExtras());
+      startActivity(resultView);
+
+    }
 
   }
+
+
 
   @Override
   protected int getListContentLayoutRes() {
